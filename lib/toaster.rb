2ab7212609ab015264a6bf1915efe2cc7e63b32d
Eric Hainer <Eric.Hainer@gmail.com>
2014-08-15 11:41:54 -0600

    Removed debugging

--- lib/toaster.rb
+++ lib/toaster.rb
@@ -16,16 +16,12 @@ module Toaster
 			opts = default_options.merge options
 			field_name = opts[:password_field].to_s.humanize
 			field_value = opts[:decrypt].call(record[opts[:password_field]])
-			puts "============================================================================"
-			puts "============================================================================"
-			puts field_value
-			puts record[opts[:password_field]]
-			puts "============================================================================"
-			record.errors[:base] << I18n.t("toaster.error.min_length", field: field_name, min: opts[:min_length]) if field_value.length < opts[:min_length]
-			record.errors[:base] << I18n.t("toaster.error.min_numeric", field: field_name, min: opts[:min_numeric]) if field_value.count("0-9") < opts[:min_numeric]
-			record.errors[:base] << I18n.t("toaster.error.min_alpha", field: field_name, min: opts[:min_alpha]) if field_value.count("A-Za-z") < opts[:min_alpha]
-			record.errors[:base] << I18n.t("toaster.error.min_punctuation", field: field_name, min: opts[:min_punctuation]) if field_value.count("^A-Za-z0-9 ") < opts[:min_punctuation]
-			record.errors[:base] << I18n.t("toaster.error.invalid_characters", field: field_name, characters: opts[:exclude].chars.join(" ")) if field_value.count(opts[:exclude]) > 0
+			record.errors << I18n.t("toaster.error.min_length", field: field_name, min: opts[:min_length]) if field_value.length < opts[:min_length]
+			record.errors << I18n.t("toaster.error.min_numeric", field: field_name, min: opts[:min_numeric]) if field_value.count("0-9") < opts[:min_numeric]
+			record.errors << I18n.t("toaster.error.min_alpha", field: field_name, min: opts[:min_alpha]) if field_value.count("A-Za-z") < opts[:min_alpha]
+			record.errors << I18n.t("toaster.error.min_punctuation", field: field_name, min: opts[:min_punctuation]) if field_value.count("^A-Za-z0-9 ") < opts[:min_punctuation]
+			record.errors << I18n.t("toaster.error.invalid_characters", field: field_name, characters: opts[:exclude].chars.join(" ")) if field_value.count(opts[:exclude]) > 0
+			puts record.errors
 		end
 	end
 end
