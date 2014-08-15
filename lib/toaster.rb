992ff76c10b2b7dd06f8af60f20ead7725cc19f3
Eric Hainer <Eric.Hainer@gmail.com>
2014-08-15 12:00:41 -0600

    Added label option for further customizing error messages

--- lib/toaster.rb
+++ lib/toaster.rb
@@ -12,9 +12,9 @@ module Toaster
 
 	class PasswordValidator < ActiveModel::Validator
 		def validate(record)
-			default_options = { password_field: :password, min_length: 8, min_numeric: 0, min_alpha: 0, min_punctuation: 0, exclude: "", decrypt: Proc.new{ |pwd| pwd } }
+			default_options = { password_field: :password, label: nil, min_length: 8, min_numeric: 0, min_alpha: 0, min_punctuation: 0, exclude: "", decrypt: Proc.new{ |pwd| pwd } }
 			opts = default_options.merge options
-			field_name = opts[:password_field].to_s.humanize
+			field_name = opts[:label].nil? ? opts[:password_field].to_s.humanize : opts[:label].to_s
 			field_value = opts[:decrypt].call(record[opts[:password_field]])
 			record.errors[:base] << I18n.t("toaster.error.min_length", field: field_name, min: opts[:min_length]) if field_value.length < opts[:min_length]
 			record.errors[:base] << I18n.t("toaster.error.min_numeric", field: field_name, min: opts[:min_numeric]) if field_value.count("0-9") < opts[:min_numeric]
