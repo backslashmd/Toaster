5fa060f77a5d2975ac3a684774afe093c5553bed
Eric Hainer <Eric.Hainer@gmail.com>
2014-08-15 12:01:04 -0600

    Added label option for further customizing error messages

--- lib/toaster.rb
+++ lib/toaster.rb
@@ -16,11 +16,11 @@ module Toaster
 			opts = default_options.merge options
 			field_name = opts[:label].nil? ? opts[:password_field].to_s.humanize : opts[:label].to_s
 			field_value = opts[:decrypt].call(record[opts[:password_field]])
-			record.errors[:base] << I18n.t("toaster.error.min_length", field: field_name, min: opts[:min_length]) if field_value.length < opts[:min_length]
-			record.errors[:base] << I18n.t("toaster.error.min_numeric", field: field_name, min: opts[:min_numeric]) if field_value.count("0-9") < opts[:min_numeric]
-			record.errors[:base] << I18n.t("toaster.error.min_alpha", field: field_name, min: opts[:min_alpha]) if field_value.count("A-Za-z") < opts[:min_alpha]
-			record.errors[:base] << I18n.t("toaster.error.min_punctuation", field: field_name, min: opts[:min_punctuation]) if field_value.count("^A-Za-z0-9 ") < opts[:min_punctuation]
-			record.errors[:base] << I18n.t("toaster.error.invalid_characters", field: field_name, characters: opts[:exclude].chars.join(" ")) if field_value.count(opts[:exclude]) > 0
+			record.errors[:toaster] << I18n.t("toaster.error.min_length", field: field_name, min: opts[:min_length]) if field_value.length < opts[:min_length]
+			record.errors[:toaster] << I18n.t("toaster.error.min_numeric", field: field_name, min: opts[:min_numeric]) if field_value.count("0-9") < opts[:min_numeric]
+			record.errors[:toaster] << I18n.t("toaster.error.min_alpha", field: field_name, min: opts[:min_alpha]) if field_value.count("A-Za-z") < opts[:min_alpha]
+			record.errors[:toaster] << I18n.t("toaster.error.min_punctuation", field: field_name, min: opts[:min_punctuation]) if field_value.count("^A-Za-z0-9 ") < opts[:min_punctuation]
+			record.errors[:toaster] << I18n.t("toaster.error.invalid_characters", field: field_name, characters: opts[:exclude].chars.join(" ")) if field_value.count(opts[:exclude]) > 0
 			puts record.errors.to_yaml
 		end
 	end
