7a9142ac5571375e16c4f5ac249245f577d6b500
Eric Hainer <Eric.Hainer@gmail.com>
2014-08-15 14:08:50 -0600

    Added upper/lower case validation option

--- lib/toaster.rb
+++ lib/toaster.rb
@@ -12,7 +12,7 @@ module Toaster
 
 	class PasswordValidator < ActiveModel::Validator
 		def validate(record)
-			default_options = { password_field: :password, label: nil, min_length: 8, min_numeric: 0, min_alpha: 0, min_punctuation: 0, exclude: "", decrypt: Proc.new{ |pwd| pwd } }
+			default_options = { password_field: :password, label: nil, min_uppercase: 0, min_lowercase: 0, min_length: 8, min_numeric: 0, min_alpha: 0, min_punctuation: 0, exclude: "", decrypt: Proc.new{ |pwd| pwd } }
 			opts = default_options.merge options
 			field_name = opts[:label].nil? ? opts[:password_field].to_s.humanize : opts[:label].to_s
 			field_value = opts[:decrypt].call(record[opts[:password_field]])
@@ -20,6 +20,8 @@ module Toaster
 			record.errors[:base] << I18n.t("toaster.error.min_numeric", field: field_name, min: opts[:min_numeric]) if field_value.count("0-9") < opts[:min_numeric]
 			record.errors[:base] << I18n.t("toaster.error.min_alpha", field: field_name, min: opts[:min_alpha]) if field_value.count("A-Za-z") < opts[:min_alpha]
 			record.errors[:base] << I18n.t("toaster.error.min_punctuation", field: field_name, min: opts[:min_punctuation]) if field_value.count("^A-Za-z0-9 ") < opts[:min_punctuation]
+			record.errors[:base] << I18n.t("toaster.error.min_uppercase", field: field_name, min: opts[:min_uppercase]) if field_value.count("A-Z") < opts[:min_uppercase]
+			record.errors[:base] << I18n.t("toaster.error.min_lowercase", field: field_name, min: opts[:min_lowercase]) if field_value.count("a-z") < opts[:min_lowercase]
 			record.errors[:base] << I18n.t("toaster.error.invalid_characters", field: field_name, characters: opts[:exclude].chars.join(" ")) if field_value.count(opts[:exclude]) > 0
 		end
 	end
