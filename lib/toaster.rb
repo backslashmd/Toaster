2bcc3322f71ed831a89744e5d6dc949418817496
Eric Hainer <Eric.Hainer@gmail.com>
2014-08-15 11:12:43 -0600

    Added decrypt param for block

--- lib/toaster.rb
+++ lib/toaster.rb
@@ -12,12 +12,15 @@ module Toaster
 
 	class PasswordValidator < ActiveModel::Validator
 		def validate(record)
-			puts record.to_yaml
-			puts params
-			default_options = { password_field: :password, min_length: 8, min_numeric: 0, min_alpha: 0, min_punctuation: 0, exclude: "" }
+			default_options = { password_field: :password, min_length: 8, min_numeric: 0, min_alpha: 0, min_punctuation: 0, exclude: "", decrypt: { |pwd| pwd } }
 			opts = default_options.merge options
 			field_name = opts[:password_field].to_s.humanize
-			field_value = record[opts[:password_field]]
+			field_value = opts[:decrypt].call(record[opts[:password_field]])
+			puts "============================================================================"
+			puts "============================================================================"
+			puts field_value
+			puts record[opts[:password_field]]
+			puts "============================================================================"
 			record.errors[:base] << I18n.t("toaster.error.min_length", field: field_name, min: opts[:min_length]) if field_value.length < opts[:min_length]
 			record.errors[:base] << I18n.t("toaster.error.min_numeric", field: field_name, min: opts[:min_numeric]) if field_value.count("0-9") < opts[:min_numeric]
 			record.errors[:base] << I18n.t("toaster.error.min_alpha", field: field_name, min: opts[:min_alpha]) if field_value.count("A-Za-z") < opts[:min_alpha]
